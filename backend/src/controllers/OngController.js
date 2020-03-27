ab97b53d00c67cb71734c9081b36b43be648f12a
Guilherme Stetes <gstetes@gmail.com>
2020-03-27 11:23:43 -0300

    Funcionalidades Avançadas Back-end

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,12 +1,12 @@
-const crypto = require('crypto')
 const connection = require('../database/connection')
+const generateUniqueId = require('../utils/generateUniqueId')
 
 module.exports = {
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body
 
-        const id = crypto.randomBytes(4).toString('HEX')
-
+        const id = generateUniqueId()
+        
         await connection('ongs').insert({
             id,
             name,
